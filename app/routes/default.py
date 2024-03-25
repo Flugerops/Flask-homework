f323132ec056c4fdb525b9a7bdac3b72b2c15adb
Flugerops <bogdan07112009@gmail.com>
2024-03-25 18:01:37 +0200

    backend done

--- app/routes/default.py
+++ app/routes/default.py
@@ -1,34 +1,42 @@
 from .. import app
-from flask import render_template
-
-@app.route("/")
-def index():    
-    return render_template("menu.html", title="Zmiini Novatori")
+from flask import render_template, url_for, redirect, request
 
 
-@app.route("/ourhobbies")
-def ourhobbies():
-    ourhobbies = [
-        ("Leonid:", " Football, Programming, Gym"),
-        ("Bohdan:", " Programming, Gym, Games"),
-        ("Nazar:", " Chess, Table Tennis, Programming"),
-        ("Oleksandr:", " Programming, Games, Gym"),
-    
-    ]
-    context = {
-        "ourhobbies" : ourhobbies,
-    }
-    return render_template("ourhobbies.html", **context)
+users = {}
 
 
-@app.route("/goodandbad")
-def goodbad():
-    goodandbad = [
-        ("Good: ", "Teamwork, Time Management, Knowledge"),
-        ("Bad: ", "404 Not Found")
-    
-    ]
-    context = {
-        "goodandbad" : goodandbad,
-    }
-    return render_template("goodandbad.html", **context)
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.get('/auth')
+def auth_get():
+    return render_template('auth.html')
+
+
+@app.post('/auth')
+def auth_post():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username]['password'] == password:
+        return render_template('welcome.html', username=username)
+    else:
+        return render_template('auth.html', message="Неправильний username або пароль!")
+
+@app.get('/register')
+def register_get():
+    return render_template('register.html')
+
+@app.post('/register')
+def register_post():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users:
+        return render_template('register.html', message="Користувач з таким username вже існує!")
+    else:
+        users[username] = {'password': password}
+        print(users)
+        return render_template('welcome.html', username=username)
